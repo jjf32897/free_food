0a2b148018301f86ee4312231f248f4634199ff1
Jo-Jo Feng <yanlin.feng@yale.edu>
2016-03-23 18:34:34 -0500

    retrieves using location, finds events with 'free food' in description or title, slightly buggy when filtering events

--- free_food/views.py
+++ free_food/views.py
@@ -1,12 +1,15 @@
 import requests, json
 from django.shortcuts import render
 from facepy import GraphAPI
+from geopy.geocoders import Nominatim
 
 def parse_event(event):
-    if 'free food' in event['description'] or 'free food' in event['name']:
-        return True
-    else:
-        return False
+    if 'description' in event:
+        if 'free food' in event['description'].lower() or 'free' in event['name'].lower():
+            return True
+
+    return False
+
 
 def index(request):
     if request.user:
@@ -14,20 +17,66 @@ def index(request):
         access_token = social.extra_data['access_token']
         graph = GraphAPI(access_token)
 
+        ids = []
+        events = []
         filtered = []
+        check = []
 
-        location = graph.get('me?fields=location')['location']['name']
-        events = graph.search(location, 'event', page=False, retry=3, center={'longitude': 40.4842, 'latitude': 88.9936}, distance=1000)
+        city = graph.get('me?fields=location')['location']['name']
+        geolocator = Nominatim()
+        location = geolocator.geocode(city)
+        coordinates = '%f,%f' % (location.latitude, location.longitude)
 
-        # query = 'Bloomington, IL'
-        # url = 'https://graph.facebook.com/search?q=%s&type=event' % query
+        # queries for places
+        places = graph.search('*', 'place', page=False, retry=3, center=coordinates, distance=10000, limit=1000, fields='name')
+
+        # query = '*'
+        # url = 'https://graph.facebook.com/search?q=%s&type=place&center=40.110539,-88.228411&distance=5000&limit=1000&fields=name' % query
         # parameters = {'access_token': access_token}
         # r = requests.get(url, params = parameters)
-        # events = json.loads(r.text)
+        # places = json.loads(r.text)
 
-        for event in events['data']:
-            # if parse_event(event):
-            filtered.append(event)
+        for place in places['data']:
+            ids.append(place['id'])
+
+        startIndex = 0
+        id_list = []
+
+        for a in range(len(ids) / 50):
+            for b in range(50):
+                id_list.append(ids[startIndex + b])
+            url = 'https://graph.facebook.com/v2.5/?ids=%s&fields=id,name,cover.fields(id,source),picture.type(large),location,events.fields(id,name,cover.fields(id,source),picture.type(large),description,start_time,attending_count,declined_count,maybe_count,noreply_count)' % ','.join(id_list)
+            parameters = {'access_token': access_token}
+            r = requests.get(url, params = parameters)
+            data = json.loads(r.text)
+
+            for place_id in data:
+                if 'events' in data[place_id]:
+                    for c in range(len(data[place_id]['events']['data'])):
+                        events.append(data[place_id]['events']['data'][c])
+
+            startIndex += 50
+            id_list = []
+
+        for i in range(len(ids) % 50):
+            id_list.append(ids[startIndex + i])
+        check.append(','.join(id_list))
+        url = 'https://graph.facebook.com/v2.5/?ids=%s&fields=id,name,cover.fields(id,source),picture.type(large),location,events.fields(id,name,cover.fields(id,source),picture.type(large),description,start_time,attending_count,declined_count,maybe_count,noreply_count)' % ','.join(id_list)
+        parameters = {'access_token': access_token}
+        r = requests.get(url, params = parameters)
+        data = json.loads(r.text)
+
+        for place_id in data:
+            if 'events' in data[place_id]:
+                for d in range(len(data[place_id]['events']['data'])):
+                    events.append(data[place_id]['events']['data'][d])
+
+        count = 0
+
+        for event in events:
+            if parse_event(event):
+                count += 1
+                filtered.append(event)
 
         return render(request, 'index.html', {'user': request.user, 'events': filtered})
 
